e117a65cdc01ebd06cf6b951d6abe47641270064
Ubuntu <azureuser@my-linux-vm.exenwdkb4ynujofqfort0y3nba.rx.internal.cloudapp.net>
2025-11-22 11:22:44 +0000

    layout.jsx created

--- Frontend/src/context/LogginContext.jsx
+++ Frontend/src/context/LogginContext.jsx
@@ -1,38 +1,3 @@
-// import { createContext, useEffect, useState } from "react";
-
-// const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//     const [isLoggedIn, setIsLoggedIn] = useState(false)
-    
-//     useEffect(() => {
-//         const savedLoggin = localStorage.getItem("isLoggedin")
-//         if (savedLoggin === "true") {
-//             setIsLoggedIn(true)
-//         }
-//     }, []);
-
-
-//     useEffect(() => {
-//         localStorage.setItem("isLoggedin", isLoggedIn)
-//     }, [isLoggedIn]);
-
-
-//     //login logout function
-
-//     const login = () => {
-//         setIsLoggedIn(true);
-//         localStorage.setItem("isLoggedIn","true")
-//     }
-
-//     const logout = () => {
-//         setIsLoggedIn(false);
-//         localStorage.removeItem("isLoggedIn");
-//         localStorage.removeItem(i)
-//     }
-// }
-
-
 
 
  import { createContext } from "react";
@@ -41,11 +6,5 @@
 
  export const dropDown = createContext();
 
-
-//import { createContext, useState, useEffect } from "react";
-import api from "../axios/axios";
-
-export const logginContext = createContext();
-export const dropDown = createContext();
 
 
